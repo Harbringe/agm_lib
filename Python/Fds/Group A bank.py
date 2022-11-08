96394592825ba72efeaf3071ea90d5db312f298f
Harbringe <99803292+Harbringe@users.noreply.github.com>
2022-11-08 08:25:13 +0530

    Update Group A bank.py

--- Python/Fds/Group A bank.py
+++ Python/Fds/Group A bank.py
@@ -1,11 +1,11 @@
-# Write a python program that computes the net amount of a bank account based on the transaction log from console input. 
-# The transaction log format is shown as following: 
-# d as deposit money
-# t as transaction money
+# Write a Python program that computes the net amount of a bank account based a
+# transaction log from console input. The transaction log format is shown as following: D
+# 100 W 200 (Withdrawal is not allowed if balance is going negative. Write functions for
+# withdraw and deposit) D means deposit while W means withdrawal.
+# Suppose the following input is supplied to the program:
+#   D 300, D 300 , W 200, D 100 Then, the output should be: 500
 
-# 1. Withdrawal is not allowed if the balance is going negative
-# 2. Write a function for withdrawal and deposit
-
+# <---Input ---> 
 d = float(input("How much would u like to deposit?\n"))
 w = float(input("How much would would u like to withdraw?\n"))
 choice = input("Do you want to deposit money or transact it?\n")
@@ -19,8 +19,8 @@ if choice == "deposit" or choice == "Deposit" or choice == "d" or choice == "D":
 elif choice == "Withdraw" or choice == "withdraw" or choice == "w" or choice == "W":
     if d > w:
         d = d - w
-        print(f"You successfully withdrew {t}. You have {d} left.")
+        print(f"You successfully withdrew {w}. You have {d} left.")
     else: 
-        print(f"Transaction ammount ({t}) is more than your account balance ({d}). You cannot transact the said amount.")
+        print(f"Transaction ammount ({w}) is more than your account balance ({d}). You cannot transact the said amount.")
 else: 
     print("Enter a valid choice")
